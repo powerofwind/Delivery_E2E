4ffc5690faa553ab86e9c82104e561982705a47a
powerofwind <powerofwind10@gmail.com>
2021-10-15 11:24:06 +0700

    example contract

---
+++ SpecFlowProject/Features/Contract.feature
@@ -0,0 +1,12 @@
+﻿@Chromium
+Feature: Contract_Management
+
+Scenario: สร้างสัญญาฉบับใหม่ ผลลัพธ์คือเมนู Restaurant มองเห็นสัญญาฉบับใหม่
+	Given  จากหน้า 'https://delivery-3rd-admin.azurewebsites.net/#/contract'
+	And กดปุ่มเพิ่มรายการ
+	When กรอกชื่อสัญญา 'สัญญาหน้าฝน'
+	When กรอกเปอร์เซ็นต์ที่หักจากร้านอาหาร '30'
+	When กรอกค่าส่ง '10'
+	Then กดปุ่มยืนยัน
+	Then แสดงสัญญาที่สร้างขึ้นมาใหม่ 'Contract : สัญญาหน้าฝน'
+	Then ในหน้า 'https://delivery-3rd-admin.azurewebsites.net/#/contract'
---
+++ SpecFlowProject/Steps/Contract_ManagementSteps.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.Steps
+{
+    [Binding]
+    public class Contract_ManagementSteps
+    {
+        private readonly IBrowser browser;
+        private IPage page;
+
+        public Contract_ManagementSteps(ScenarioContext scenarioContext)
+        {
+            browser = scenarioContext.Get<IBrowser>();
+        }
+
+        [Given(@"จากหน้า '(.*)'")]
+        public async Task Givenจากหนา(string url)
+        {
+            page ??= await browser.NewPageAsync();
+            await page.GotoAsync(url);
+        }
+        
+        [Given(@"กดปุ่มเพิ่มรายการ")]
+        public async Task Givenกดปเพมรายการ()
+        {
+            //await page.RunAndWaitForNavigationAsync(async () =>
+            //{
+            //    await page.ClickAsync("text=เพิ่มรายการ >> span");
+            //});
+            await page.ClickAsync("text=เพิ่มรายการ >> span");
+        }
+        
+        [When(@"กรอกชื่อสัญญา '(.*)'")]
+        public async Task Whenกรอกชอสญญา(string contractName)
+        {
+            await page.FillAsync("input[name=\"ion-input-0\"]", contractName);
+        }
+
+        [When(@"กรอกเปอร์เซ็นต์ที่หักจากร้านอาหาร '(.*)'")]
+        public async Task Whenกรอกเปอรเซนตทหกจากรานอาหาร(string percentage)
+        {
+            await page.FillAsync("input[name=\"ion-input-1\"]", percentage);
+        }
+
+        [When(@"กรอกค่าส่ง '(.*)'")]
+        public async Task Whenกรอกคาสง(string deliveryfee)
+        {
+            await page.FillAsync("input[name=\"ion-input-2\"]", deliveryfee);
+        }
+
+        [Then(@"กดปุ่มยืนยัน")]
+        public async Task Thenกดปมยนยน()
+        {
+            await page.ClickAsync("text=ยืนยัน >> button");
+        }
+
+        [Then(@"แสดงสัญญาที่สร้างขึ้นมาใหม่ '(.*)'")]
+        public async Task Thenแสดงสญญาทสรางขนมาใหม(string contractName)
+        {
+            var actual = await page.InnerTextAsync("text=Contract : สัญญาหน้าฝน");
+            actual.Should().BeEquivalentTo(contractName);
+        }
+
+        [Then(@"ในหน้า '(.*)'")]
+        public void Thenในหนา(string urlExpect)
+        {
+            page.Url.Should().BeEquivalentTo(urlExpect);
+        }
+
+    }
+}
